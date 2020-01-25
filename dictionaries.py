e92c611f8798fbffe1ade3b03a8396ee61290e17
Emilio Araos <ea3dev@gmail.com>
2020-01-25 03:55:18 -0700

    get method for dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -11,9 +11,19 @@ print(fruit["lemon"])
 fruit["pear"] = "an odd shape apple"
 fruit["mango"] = "a sweat pleasure"
 fruit["zapote"] = "red inside"
-fruit[4] = "A number"
-fruit[("a car", "a house")] = "this is a tuple"
+# fruit[4] = "A number"
+# fruit[("a car", "a house")] = "this is a tuple"
+# print(fruit)
+# del fruit[4]
+# fruit.clear()
+# print(fruit)
 print(fruit)
-del fruit[4]
-print(fruit)
-
+while True:
+    dict_key = input("Please enter a fruit: ")
+    if dict_key == "quit":
+        break
+    if dict_key in fruit:
+        description = fruit.get(dict_key)
+        print(description)
+    else:
+        print("We don't have a " + dict_key)
