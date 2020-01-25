d25f4bb94670e249f6ae0c6b615b9aef06bfc839
Emilio Araos <ea3dev@gmail.com>
2020-01-25 04:18:14 -0700

    iterating the dict

--- dictionaries.py
+++ dictionaries.py
@@ -22,8 +22,30 @@ while True:
     dict_key = input("Please enter a fruit: ")
     if dict_key == "quit":
         break
+    #
+    # description = fruit.get(dict_key,"We don't have a " + dict_key )
+    # print(description)
     if dict_key in fruit:
         description = fruit.get(dict_key)
         print(description)
     else:
         print("We don't have a " + dict_key)
+
+for snack in fruit:
+    print(fruit[snack])
+
+for i in range(10):
+    for snack in fruit:
+        print(snack + " is " + fruit[snack])
+    print("-"*40)
+
+ordered_key = list(fruit.keys())
+print(ordered_key)
+ordered_key.sort()
+print(ordered_key)
+for i in ordered_key:
+    print(i + " - " + fruit[i])
+
+
+
+
