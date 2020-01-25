7dfbb800e22763086e4ceedad5a9be01a7db08a7
Emilio Araos <ea3dev@gmail.com>
2020-01-25 04:58:38 -0700

    cave game

---
+++ cavegame.py
@@ -0,0 +1,14 @@
+locations = {0: "You are sitting in the front of your computer",
+             1: "You are standing at the end of a road before a small brick building",
+             2: "You are the top of the hill",
+             3: "You are inside a building a well house for a small stream",
+             4: "You are in a valley beside a stream",
+             5: " You are in the forest"}
+
+exits = [{"Q": 0},
+         {"W": 2, "E": 3, "N": 5,"S": 4, "Q": 0},
+         {"N": 5, "Q":0},
+         {"W": 1, "Q":0},
+         {"N": 1, "W":2, "Q":0},
+         {"W":2, "S":1, "Q":0}]
+
---
+++ join.py
@@ -0,0 +1,10 @@
+my_list = ["a", "b", "c", "d"]
+new_string = ", ".join(my_list)
+print(new_string)
+letters = "abcdefghijklmnopqrstuvwxyz"
+new_string2 = " - ".join(letters)
+list1 = list(new_string2)
+
+print(list1)
+
+
